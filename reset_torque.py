6ad16e11432f84dabc77520c0798a4a38b125173
Graunephar <daniel@graungaard.com>
2017-11-25 17:23:32 +0100

    Reset added

--- reset_torque.py
+++ reset_torque.py
@@ -10,3 +10,13 @@ print('ports found', ports)
 print('connecting on the first available port:', ports[0])
 dxl_io = pypot.dynamixel.DxlIO(ports[0])
 
+found_ids = dxl_io.scan(range(10))
+
+print("%s found IDS" % found_ids)
+
+led = dxl_io.is_led_on(found_ids)
+
+print("LED", led)
+
+dxl_io.enable_torque(found_ids)
+
